426b0e4705a1adddf1503758d086f6be345ed36d
Maks-Ytka <maksym.kozlovskiy@gmail.com>
2024-01-15 00:32:15 +0200

    Added correct curly braces for condition (if)

--- Tasks/count.js
+++ Tasks/count.js
@@ -6,7 +6,9 @@ const count = (obj) => {
 
   keys.forEach((key) => {
     const value = obj[key];
-    if (typeof value === 'number') sum += value;
+    if (typeof value === 'number') {
+      sum += value;
+    }
   });
 
   return sum;
