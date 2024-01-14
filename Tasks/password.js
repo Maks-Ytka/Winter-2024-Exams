d7088a637b79583ecdd1d45ef35324334564e7de
Maks-Ytka <maksym.kozlovskiy@gmail.com>
2024-01-15 01:59:29 +0200

    use const and let

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,10 +1,12 @@
 // Generate random password
 //Renamed function and variables
-let generatePassword = (alphabet, length) => {
+//use const and let
+const generatePassword = (alphabet, length) => {
   const maxIndex = alphabet.length;
-  key = '';
+  let key = '';
+
   for (let i = 0; i < length; i++) {
-    randomIndex = Math.floor(Math.random() * maxIndex);
+    const randomIndex = Math.floor(Math.random() * maxIndex);
     key = key + alphabet[randomIndex];
   }
   return key;
