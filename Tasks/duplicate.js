947de50fa97efc0aba84da590624eaa1503c8723
Maks-Ytka <maksym.kozlovskiy@gmail.com>
2024-01-15 03:01:39 +0200

    simplifying the creation of an array with repeated values

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,6 +1,7 @@
 // Return an array without duplicates
 //Renamed function
 //use const and removed unnecessary else
+//Used Array(N).fill(value) to simplify the creation of an array with duplicate values.
 const createDuplicateArray = (value, N) => {
   if (N <= 0) return [];
 
