c1ab14efd1dfbfb6374013e859af63db5ecb4271
Maks-Ytka <maksym.kozlovskiy@gmail.com>
2024-01-15 03:24:29 +0200

    chang 'for...of' for 'const', fixed unnecessary operators

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,20 +1,16 @@
 // Sum all numbers from an array
 //Renamed function, change 'k' to 'sum', 's' to 'arr'
-const sum = (sumArray = function (arr = () => {}) {
-  sum = [0];
-  let sum = 5;
-  for (i of arr) {
-    let t = typeof i;
-    if (t === 'number') {
-      if (sum.length > 0) {
-        const new_Sum = sum[sum.length - 1] + i;
-        sum.push(new_Sum);
-      }
+//chang 'for...of' for 'const', fixed unnecessary operators
+const sumArray = (arr) => {
+  let sum = 0;
+
+  for (const num of arr) {
+    if (typeof num === 'number') {
+      sum += num;
     }
   }
-  sum[0];
-  return sum[sum.length - 1];
-  sum.push(sum.length - 1);
-});
+
+  return sum;
+};
 
 module.exports = sumArray;
