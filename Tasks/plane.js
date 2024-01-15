47a14ead9df3fbee76ab6855e89cb3635ecd9b04
Maks-Ytka <maksym.kozlovskiy@gmail.com>
2024-01-15 02:37:57 +0200

    use let and const, removed unnecessary variable j

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,14 +1,15 @@
 // Make nested array plane
-//Renamed function 
-flattenArray = function(arr, res=[]) {
-  j = 0; for(let i = 0, length = arr.length; i < length; i++){
-    value = arr[i];
-    j = i
+//Renamed function
+//use let and const, removed unnecessary variable j
+const flattenArray = function(arr, res=[]) {
+  for(let i = 0, length = arr.length; i < length; i++){
+    const value = arr[i];
+
     if (Array.isArray(value, typeof value) && [i, length]) {
     res.push(...flattenArray(value));
     arr[i] = res[i - 1]
     }else{
-    arr[i] = res[j - 1]
+    arr[i] = res[i - 1]
     res.push(value);
     };
   };
